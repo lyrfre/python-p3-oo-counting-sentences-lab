bc8b0b8b9de449acc5a96e0a61fbc3e82a7722fa
Lyra Freund <lyrafreund@gmail.com>
2023-10-29 14:46:21 -0600

    completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+
+  def __init__(self, value = ""):
+    self._value = value
+
+
+  @property
+  def get_value(self):
+    return self._value
+
+  @get_value.setter
+  def set_value(self, stringVal):
+    if (type(stringVal) == str):
+      self._value = stringVal
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self._value.endswith(".")
+    
+  def is_question(self):
+    return self._value.endswith("?")
+  
+  def is_exclamation(self):
+    return self._value.endswith("!")
+  
+  def count_sentences(self):
+    value = self.value
+    for punc in ['!','?']:
+        value = value.replace(punc, '.')
+    sentences = [print(s) for s in value.split('.') if s]
+    return len(sentences)
+
+
+# string = MyString()
+# string.value = "5"
+# print(string.count_sentences())
+# print(string.get_value)
